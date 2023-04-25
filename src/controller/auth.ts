69246ce26c7abd4b39cbd700c01b9bb2da97415c
RJD02 <dulangeraviraj@gmail.com>
2023-04-26 00:47:28 +0530

    Resolve bugs

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -7,6 +7,7 @@ import { Roles } from "../utils/types";
 import { checkPassword, hashPassword } from "../utils/hashing";
 import jwt from "jsonwebtoken";
 import { UniqueOTP } from "../utils/parentKeyGenerator";
+import { Document } from "mongoose";
 
 export const loginController = async (req: Request, res: Response) => {
   const { username, password, type, email } = req.body;
@@ -24,10 +25,10 @@ export const loginController = async (req: Request, res: Response) => {
       user = await Parent.findOne({ email });
     }
     console.log(user);
-    if (user === null) {
+    if (!user) {
       return res.status(404).json({ message: "User not found, maybe signup?" });
     }
-    if (!checkPassword(password, user.password)) {
+    if (user && !checkPassword(password, user.password)) {
       return res.status(401).json({ message: "Passwords don't match" });
     }
     const token = jwt.sign(
@@ -62,10 +63,11 @@ export const signupController = async (req: Request, res: Response) => {
     }
     if (user != null)
       return res.status(200).json({ message: "email already exists" });
-    let newUser = null;
+
     if (!password || !email || !type || !name)
       return res.status(402).json({ message: "please fill all credentials" });
     const encryptedPassword = hashPassword(password);
+    let newUser = null;
     if (type === Roles.STUDENT) {
       if (!parentKey || !teacherKey)
         return res
@@ -74,15 +76,18 @@ export const signupController = async (req: Request, res: Response) => {
       const parent = await Parent.findOne({ parentKey });
       const teacher = await Teacher.findOne({ teacherKey });
       if (!parent || !teacher)
-        res
+        return res
           .status(403)
           .json({ message: "No parent/teacher with that parent code exists" });
-      newUser = await new Student({
+      newUser = new Student({
         username,
         password: encryptedPassword,
         email,
         name,
       });
+
+      if (!newUser)
+        return res.status(500).json({ message: "something went wrong" });
       newUser.parent = parent._id;
       newUser.teacher = teacher._id;
       await newUser.save();
@@ -108,7 +113,7 @@ export const signupController = async (req: Request, res: Response) => {
       });
     } else if (type === Roles.PARENT) {
       const parentKey = UniqueOTP(10);
-      newUser = await new Parent({
+      newUser = new Parent({
         username,
         password: encryptedPassword,
         email,
